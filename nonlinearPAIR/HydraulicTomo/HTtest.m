52bf66a1286a8d9c6c9f9e080b21add35dab5a21
Emm Hart <ehart5@emory.edu>
2024-03-06 08:13:50 -0500

    Read and Created HT images

--- nonlinearPAIR/HydraulicTomo/HTtest.m
+++ nonlinearPAIR/HydraulicTomo/HTtest.m
@@ -1,9 +1,9 @@
 %% Load Existing Hydrolic Tomography Data
-for i=1:10
+for i=1:100
         load(append('data/inputHT',string(i),'.mat'))
         load(append('data/targetHT',string(i),'.mat'))
     for j=1:100
-        imwrite(input(:,:,j),append('images/train/input',string((i-1)*100+j),'.png'),'PNG')
-        imwrite(target(:,:,j),append('images/train/target',string((i-1)*100+j),'.png'),'PNG')
+        imwrite(input(:,:,j),append('images/input',string((i-1)*100+j),'.png'),'PNG')
+        imwrite(target(:,:,j),append('images/target',string((i-1)*100+j),'.png'),'PNG')
     end
 end
